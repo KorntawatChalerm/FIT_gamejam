4be6fc18f0b34f577049aaf3a9d9907b298ee6ee
KorntawatChalerm <108566878+KorntawatChalerm@users.noreply.github.com>
2022-07-03 14:42:49 +0700

    add player 2 in progress
    -player 2 movement is using the same key as player 1

---
+++ Assets/Script/Player2Controller.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Controller : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody rb;
+    [SerializeField]
+    private float speed = 10f;
+    [SerializeField]
+    private float jumpforce = 10f;
+
+    bool onladder = false;
+
+
+    void Start()
+    {
+        //   rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        //moving horizontal
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
+        /* var horizontal = Input.GetAxis("Horizontal");
+         var vertical = Input.GetAxis("Vertical");
+
+         transform.position +=  new Vector3(horizontal, 0) * Time.deltaTime * speed;*/
+
+        //jump
+        if (Input.GetKeyDown("up") && Mathf.Abs(rb.velocity.y) <= 0.3 && onladder == false)
+        {
+
+            rb.AddForce(new Vector2(0, jumpforce * 1000));
+
+        }
+        if (Input.GetKey("up") && onladder)
+        {
+            Debug.Log("climbing");
+            rb.velocity = new Vector2(0, 5);
+
+            // rb.AddForce(new Vector2(0, 20));
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("ladder"))
+        {
+            Debug.Log("ladder");
+            onladder = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("ladder"))
+        {
+            Debug.Log("ladderoff");
+            onladder = false;
+
+        }
+
+
+    }
+}
